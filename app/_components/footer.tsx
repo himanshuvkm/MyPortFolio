268a143a2f49c0bf62d7fbfce78b4a43954132a3
himanshuvkm <himanshuvkm252@gmail.com>
2025-12-12 23:48:38 +0530

    fix security issues

--- app/_components/footer.tsx
+++ app/_components/footer.tsx
@@ -27,52 +27,56 @@ export default function Footer() {
             href="mailto:himanshuvkm252@gmail.com"
             target="_blank"
             className="
-                       flex items-center gap-2 px-4 py-2 text-sm 
-                       bg-[var(--muted)] border border-[var(--border)] rounded-lg
-                       hover:bg-[color-mix(in oklch, var(--muted), black 10%)]
-                       transition-all
-                     "
+    flex items-center gap-2 px-4 py-2 text-sm 
+    bg-[var(--muted)] border border-[var(--border)] rounded-lg
+    hover:bg-[color-mix(in oklch, var(--muted), black 10%)]
+    transition-all
+  "
           >
-            <Mail size={16} /> Email
+            <Mail size={16} />
+            <span className="hidden sm:inline">Email</span>
           </Link>
 
           <Link
             href="https://github.com/himanshuvkm"
             target="_blank"
             className="
-                       flex items-center gap-2 px-4 py-2 text-sm 
-                       bg-[var(--muted)] border border-[var(--border)] rounded-lg
-                       hover:bg-[color-mix(in oklch, var(--muted), black 10%)]
-                       transition-all
-                     "
+    flex items-center gap-2 px-4 py-2 text-sm 
+    bg-[var(--muted)] border border-[var(--border)] rounded-lg
+    hover:bg-[color-mix(in oklch, var(--muted), black 10%)]
+    transition-all
+  "
           >
-            <Github size={16} /> GitHub
+            <Github size={16} />
+            <span className="hidden sm:inline">GitHub</span>
           </Link>
 
           <Link
             href="https://x.com/Himanshu_10147"
             target="_blank"
             className="
-                       flex items-center gap-2 px-4 py-2 text-sm 
-                       bg-[var(--muted)] border border-[var(--border)] rounded-lg
-                       hover:bg-[color-mix(in oklch, var(--muted), black 10%)]
-                       transition-all
-                     "
+    flex items-center gap-2 px-4 py-2 text-sm 
+    bg-[var(--muted)] border border-[var(--border)] rounded-lg
+    hover:bg-[color-mix(in oklch, var(--muted), black 10%)]
+    transition-all
+  "
           >
-            <SiX size={14} className="" /> Twitter
+            <SiX size={14} />
+            <span className="hidden sm:inline">Twitter</span>
           </Link>
 
           <Link
             href="https://www.linkedin.com/in/himanshu-vishwakarma-2275a5354"
             target="_blank"
             className="
-                       flex items-center gap-2 px-4 py-2 text-sm 
-                       bg-[var(--muted)] border border-[var(--border)] rounded-lg
-                       hover:bg-[color-mix(in oklch, var(--muted), black 10%)]
-                       transition-all
-                     "
+    flex items-center gap-2 px-4 py-2 text-sm 
+    bg-[var(--muted)] border border-[var(--border)] rounded-lg
+    hover:bg-[color-mix(in oklch, var(--muted), black 10%)]
+    transition-all
+  "
           >
-            <Linkedin size={16} /> LinkedIn
+            <Linkedin size={16} />
+            <span className="hidden sm:inline">LinkedIn</span>
           </Link>
         </div>
       </div>
@@ -83,7 +87,7 @@ export default function Footer() {
       </p>
 
       {/* SIGNATURE */}
-      <p className="italic text-[var(--graytext)] text-sm mt-10">
+      <p className="italic text-[var(--graytext)] text-sm mt-1">
         Designed & Made with <span className="text-pink-500">❤️</span>
       </p>
 
